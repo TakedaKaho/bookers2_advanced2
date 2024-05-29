62c11e3dd34fcce7bf8cd339cca38aaaab8dbeb9
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-05-29 10:12:04 +0000

    [Finish]応用課題４

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,3 +2,5 @@ class Relationship < ApplicationRecord
     belongs_to :follower, class_name: "User"
     belongs_to :followed, class_name: "User"
 end
+
+#わからなくなったらhttps://zenn.dev/ganmo3/articles/a3633e8f3209da 
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,12 @@ class User < ApplicationRecord
     active_relationships.find_by(followed_id: user.id).destroy
   end
   
+    # 指定したユーザーをフォローしているかどうかを判定
+  def following?(user)
+    followings.include?(user)
+  end
+
+  
   
   has_one_attached :profile_image
 
