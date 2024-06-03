3536b0dabcd5ca3caecf29e5ec9c57a72a5cbf4e
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-06-03 02:09:14 +0000

    応用課題７a 並び替えの非同期化は未実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,10 @@ class BooksController < ApplicationController
   end
 
   def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @book_favorites = Book.includes(:favorites).sort_by { |book| -book.favorites.where(created_at: from...to).count }
+    
     @books = Book.all
     @book = Book.new
   end
