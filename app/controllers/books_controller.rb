11edc5985eeeb83e0d14ef3ce4c1b65102978b4d
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-06-04 14:43:33 +0000

    応用課題９a実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,13 +17,14 @@ class BooksController < ApplicationController
     @user = @book.user
     @book_new = Book.new
     @book_comment = BookComment.new
+    @book.increment!(:view_count)
   end
 
   def index
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
     @book_favorites = Book.includes(:favorites).sort_by { |book| -book.favorites.where(created_at: from...to).count }
-    
+    @books_count = Book.all
     @books = Book.all
     @book = Book.new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_04_003626) do
+ActiveRecord::Schema.define(version: 2024_06_04_141538) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2024_06_04_003626) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "view_count"
   end
 
   create_table "chats", force: :cascade do |t|
@@ -71,6 +72,15 @@ ActiveRecord::Schema.define(version: 2024_06_04_003626) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "read_counts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_read_counts_on_book_id"
+    t.index ["user_id"], name: "index_read_counts_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -112,6 +122,8 @@ ActiveRecord::Schema.define(version: 2024_06_04_003626) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chats", "rooms"
   add_foreign_key "chats", "users"
+  add_foreign_key "read_counts", "books"
+  add_foreign_key "read_counts", "users"
   add_foreign_key "user_rooms", "rooms"
   add_foreign_key "user_rooms", "users"
 end
