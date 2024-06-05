11e4c85033600205e2bffccda98679624f96433d
TakedaKaho <dummy@example.com>
2024-06-05 13:36:32 +0000

    応用課題７d

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_04_141538) do
+ActiveRecord::Schema.define(version: 2024_06_05_103210) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2024_06_04_141538) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "view_count"
+    t.string "star"
   end
 
   create_table "chats", force: :cascade do |t|
